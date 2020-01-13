1386c2b88f578b7cdb99c085d9105aaa8e7f2025
Raymond Namar <namar.ra@husky.neu.edu>
2020-01-13 12:27:34 -0500

    updated part4

--- lab0_setup/part4/example.cpp
+++ lab0_setup/part4/example.cpp
@@ -1,5 +1,3 @@
-// A first C++ program
-// Standard libraries that we include.
 #include <iostream>
 #include <string>
 
@@ -7,33 +5,23 @@
 int main()
 {
 
-// ========================== for - loops ====================
      // Allocate an array on the stack.
      // This array cannot be referenced outside the main function.
      int myArray[10];
 
-     // A loop that pre-increments 'i' by one each iteration of the
-     // loop.
+     // counted-for loop that pre-increments 'i' by one each iteration of the
      for(int i =0; i < 10 ; ++i) {
         // If we are modifying values , then use a regular for-loop
         myArray[i] = i ;
         // cout is also part of the standard library, and prints
-        // text to standard output (i.e. typically our terminal window)
         std::cout << i << " ";
      }
 
-     // Here we create a string type. It is part of the
-     // standard library namespace, which is why it begins with
-     // std::
      std::string s= "hello";
 
-     // Here is a different kind of loop.
+     // for-each loop.
      for(char c : s) {
-     // Range-based for loops are useful if we are not modifying any values.
-     // Ranged-based for loops were introduced in C++11 and are a new popular feature
-     // in many other programming languages, and now C++
         std::cout << c ;
      }
 
-     // More information can be found here: http://en.cppreference.com/w/cpp/language/range-for
 }
