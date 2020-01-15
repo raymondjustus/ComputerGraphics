fa856517cad8e1966ddb83bad55e5162ab1e639d
Raymond Namar <namar.ra@husky.neu.edu>
2020-01-14 20:59:41 -0500

    Adding lab 1

--- Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
+++ Lab1_C_Plus_Plus_SoftwareRasterizerPart1/main.cpp
@@ -23,6 +23,7 @@
 
 // C++ Standard Libraries
 #include <iostream>
+#include <algorithm>
 
 // User libraries
 #include "GL.h"
@@ -61,6 +62,10 @@ void drawLine(Vec2 v0, Vec2 v1, TGA& image, ColorRGB c){
     }
 }
 
+float crossProduct(Vec2 a, Vec2 b) {
+	return (a.x * b.y) - (a.y * b.x);
+}
+
 // Draw a triangle
 void triangle(Vec2 v0, Vec2 v1, Vec2 v2,TGA& image, ColorRGB c){
     if(glFillMode==LINE){
@@ -69,6 +74,33 @@ void triangle(Vec2 v0, Vec2 v1, Vec2 v2,TGA& image, ColorRGB c){
         drawLine(v2,v0,image,c);
     }
     // TODO: Draw a filled triangle
+	
+	//make a bounding box
+	int maxX = std::max(v0.x, std::max(v1.x, v2.x));
+	int minX = std::min(v0.x, std::min(v1.x, v2.x));
+	int maxY = std::max(v0.y, std::max(v1.y, v2.y));
+	int minY = std::min(v0.y, std::min(v1.y, v2.y));
+
+	/* spanning vectors of edge (v1,v2) and (v1,v3) */
+	Vec2 vs1 = Vec2(v1.x - v0.x, v1.y - v0.y);
+	Vec2 vs2 = Vec2(v2.x - v0.x, v2.y - v0.y);
+
+	for (int x = minX; x <= maxX; x++)
+	{
+		for (int y = minY; y <= maxY; y++)
+		{
+			Vec2 q = Vec2(x - v0.x, y - v0.y);
+
+			float s = (float)crossProduct(q, vs2) / crossProduct(vs1, vs2);
+			float t = (float)crossProduct(vs1, q) / crossProduct(vs1, vs2);
+
+			if ((s >= 0) && (t >= 0) && (s + t <= 1))
+			{ /* inside triangle */
+				canvas.setPixelColor(x, y, c);
+			}
+		}
+	}
+
 }
 
 
@@ -79,22 +111,33 @@ int main(){
     // A sample of color(s) to play with
     ColorRGB red;
     red.r = 255; red.g = 0; red.b = 0;
-        
     
+	ColorRGB blue;
+	blue.r = 0; blue.g = 0; blue.b = 255;
+
+	ColorRGB green;
+	green.r = 0; green.g = 255; green.b = 0;
+
     // Points for our Line
     Vec2 line[2] = {Vec2(0,0), Vec2(100,100)};
 
     // Set the fill mode
-    glPolygonMode(FILL);
+    glPolygonMode(LINE);
 
     // Draw a line
     drawLine(line[0],line[1],canvas,red);
 
-    // Data for our triangle
+    // Data for our triangles
     Vec2 tri[3] = {Vec2(160,60),Vec2(150,10),Vec2(75,190)};
+	Vec2 tri2[3] = { Vec2(-160,60),Vec2(-150,10),Vec2(-75,190) };
+	Vec2 tri3[3] = { Vec2(160,160),Vec2(150,180),Vec2(75,200) };
+
+
 
     // Draw a triangle
     triangle(tri[0],tri[1],tri[2],canvas,red);
+	triangle(tri2[0], tri2[1], tri2[2], canvas, blue);
+	triangle(tri3[0], tri3[1], tri3[2], canvas, green);
 
     // Output the final image
     canvas.outputTGAImage("graphics_lab2.ppm");
